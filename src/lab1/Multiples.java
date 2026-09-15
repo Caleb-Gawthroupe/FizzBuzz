779c9fccd93a9b40ceab38ca9c1af9fd7f300064
Caleb Gawthroupe <gawthroupeca@gmail.com>
2026-09-15 14:17:16 -0400

    refactored multiples method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,18 @@
 package lab1;
 
 public class Multiples {
+    static void main() {
+        int answer = multiples();
+        System.out.println(answer);
+    }
+
+    private static int multiples() {
+        int count = 0;
+        for (int i = 1; i < 1000; i++){
+            if (i % 3 == 0 || i % 5 == 0){
+                count++;
+            }
+        }
+        return count;
+    }
 }
