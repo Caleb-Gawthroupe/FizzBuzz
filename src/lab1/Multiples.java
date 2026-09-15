4c953526e0b47217e4cd4d861e525dd3d5a257f5
Caleb Gawthroupe <gawthroupeca@gmail.com>
2026-09-15 14:22:03 -0400

    refactored for multiples test

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,14 +2,14 @@ package lab1;
 
 public class Multiples {
     static void main() {
-        int answer = multiples();
+        int answer = multiples(1000, 3, 5);
         System.out.println(answer);
     }
 
-    private static int multiples() {
+    public static int multiples(int a, int b, int c) {
         int count = 0;
-        for (int i = 1; i < 1000; i++){
-            if (i % 3 == 0 || i % 5 == 0){
+        for (int i = 1; i < a; i++){
+            if (i % b == 0 || i % c == 0){
                 count++;
             }
         }
